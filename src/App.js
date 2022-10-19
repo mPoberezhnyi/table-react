177e02164eb1904851c351cb91c89852cb36a5ce
Mykola Poberezhnyi <mpoberezhnyi.externe@bedrockstreaming.com>
2022-10-19 10:52:58 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ export default function App() {
 
 	const [state, setState] = useState(arr);
 
+    
+
 	const increase = (yIndex, xIndex) => {
         const currentNumber = state[yIndex][xIndex];
         const newState = [...state];
